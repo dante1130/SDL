3702a637c5264e22ceacbc678cf490c850c865c3
dante1130 <andrewdante2@gmail.com>
2024-12-11 19:24:35 +0800

    install the dll

--- build.zig
+++ build.zig
@@ -28,4 +28,6 @@ pub fn build(b: *std.Build) void {
 
     build_sdl_with_cmake.step.dependOn(&configure_sdl_with_cmake.step);
     b.getInstallStep().dependOn(&build_sdl_with_cmake.step);
+
+    b.installBinFile("lib/bin/SDL3.dll", "SDL3.dll");
 }
