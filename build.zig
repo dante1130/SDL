eed8b512d3a6c03a2477ca2b8e65575094b67714
dante1130 <andrewdante2@gmail.com>
2024-12-11 17:28:50 +0800

    Revert "Builld SDL with zig"
    This reverts commit 9c7d447567fe8527ec2650ff5d62707587d2deb6.

--- build.zig
+++ build.zig
@@ -1,29 +1,18 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
-    const cwd = try std.fs.cwd().realpathAlloc(b.allocator, ".");
-
-    const absolute_zig_cc_cmd_path = try std.fs.path.resolve(b.allocator, &[_][]const u8{
-        cwd,
-        "cmake/zig-cc.cmd",
-    });
-
-    const cmake_c_compiler_str = try std.fmt.allocPrint(b.allocator, "-DCMAKE_C_COMPILER={s}", .{absolute_zig_cc_cmd_path});
-
+pub fn build(b: *std.Build) void {
     const configure_sdl_with_cmake = b.addSystemCommand(&[_][]const u8{
         "cmake",
         "-S",
         ".",
         "-B",
         "build",
-        cmake_c_compiler_str,
-        "-DCMAKE_GENERATOR=Ninja",
         "-DCMAKE_BUILD_TYPE=Release",
         "-DCMAKE_INSTALL_PREFIX=lib",
         "-DSDL_SHARED=OFF",
         "-DSDL_STATIC=ON",
         "-DSDL_TEST_LIBRARY=OFF",
-        "-DSDL_TESTS=OFF",
+        "-DSDL_TEST=OFF",
         "-DSDL_INSTALL_TESTS=OFF",
     });
 
